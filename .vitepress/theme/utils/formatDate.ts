e1f1598f4ea35aeba0eba7de07e3eb917d40eb20
mug1wara26 <aloysiusgsq@gmail.com>
2025-12-03 20:07:16 +0800

    Serve static files

--- .vitepress/theme/utils/formatDate.ts
+++ .vitepress/theme/utils/formatDate.ts
@@ -1,7 +1,7 @@
 export default function (date: string) {
   return new Date(date).toLocaleString(undefined, {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit'
+    day: "numeric",
+    month: "long",
+    year: "numeric",
   });
 }
